46f72201d64a5002fe9775cad1f2172446c9bd35
Lochie Axon <lochieaxon@gmail.com>
2026-02-17 02:03:46 +1100

    fix: init spinner animation

--- site/src/components/homepage/examples/action.tsx
+++ site/src/components/homepage/examples/action.tsx
@@ -15,23 +15,25 @@ const states = [
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <motion.g
-          animate={{ rotate: 360 }}
-          transition={{ repeat: Infinity, duration: 0.5, ease: "linear" }}
-        >
-          <path
-            d="M21.313 11.4062C21.313 16.8775 16.8777 21.3128 11.4065 21.3128"
-            stroke="currentColor"
-            strokeWidth="3"
-            strokeLinecap="round"
-          />
-          <path
-            opacity="0.1"
-            d="M11.4065 21.313C16.8777 21.313 21.313 16.8777 21.313 11.4065C21.313 5.93529 16.8777 1.5 11.4065 1.5C5.93529 1.5 1.5 5.93529 1.5 11.4065C1.5 16.8777 5.93529 21.313 11.4065 21.313Z"
-            stroke="currentColor"
-            strokeWidth="3"
-          />
-        </motion.g>
+        <AnimatePresence>
+          <motion.g
+            animate={{ rotate: 360 }}
+            transition={{ repeat: Infinity, duration: 0.5, ease: "linear" }}
+          >
+            <path
+              d="M21.313 11.4062C21.313 16.8775 16.8777 21.3128 11.4065 21.3128"
+              stroke="currentColor"
+              strokeWidth="3"
+              strokeLinecap="round"
+            />
+            <path
+              opacity="0.1"
+              d="M11.4065 21.313C16.8777 21.313 21.313 16.8777 21.313 11.4065C21.313 5.93529 16.8777 1.5 11.4065 1.5C5.93529 1.5 1.5 5.93529 1.5 11.4065C1.5 16.8777 5.93529 21.313 11.4065 21.313Z"
+              stroke="currentColor"
+              strokeWidth="3"
+            />
+          </motion.g>
+        </AnimatePresence>
       </svg>
     ),
   },
@@ -87,7 +89,7 @@ export const ExampleAction = () => {
   return (
     <div className={styles.action}>
       <div className={styles.iconWrapper}>
-        <AnimatePresence>
+        <AnimatePresence initial={false}>
           <motion.div
             key={currentStateIndex}
             className={styles.icon}
